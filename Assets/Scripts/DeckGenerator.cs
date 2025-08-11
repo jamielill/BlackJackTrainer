b90c6ea715d85cd78019d444cbf0eac7bdfe5b8b
jamielill <jl2592c@gre.ac.uk>
2025-08-11 17:21:24 +0100

    added hit and stand buttons and a 3rd player card

--- Assets/Scripts/DeckGenerator.cs
+++ Assets/Scripts/DeckGenerator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
@@ -7,6 +8,8 @@ using UnityEngine.UI;
 public class DeckGenerator : MonoBehaviour
 {
     [SerializeField] private static int numberOfDecks = 6;
+    [SerializeField] private float dealSpeed = 0.5f;
+
     private List<string> upcomingCards = new List<string>();
     private static string[] cards = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
     private static string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
@@ -14,16 +17,19 @@ public class DeckGenerator : MonoBehaviour
     public event Action<int> OnCardAmountChanged;
     [SerializeField] private GameObject playerCard0;
     [SerializeField] private GameObject playerCard1;
+    [SerializeField] private GameObject playerCard2;
     [SerializeField] private GameObject dealerCard0;
     [SerializeField] private GameObject dealerCard1;
 
     private TMPro.TextMeshProUGUI playerCard0Text;
     private TMPro.TextMeshProUGUI playerCard1Text;
+    private TMPro.TextMeshProUGUI playerCard2Text;
     private TMPro.TextMeshProUGUI dealerCard0Text;
     private TMPro.TextMeshProUGUI dealerCard1Text;
 
     private UnityEngine.UI.Image playerCard0Image;
     private UnityEngine.UI.Image playerCard1Image;
+    private UnityEngine.UI.Image playerCard2Image;
     private UnityEngine.UI.Image dealerCard0Image;
     private UnityEngine.UI.Image dealerCard1Image;
 
@@ -32,11 +38,13 @@ public class DeckGenerator : MonoBehaviour
     {
         playerCard0Text = playerCard0.GetComponentInChildren<TMPro.TextMeshProUGUI>();
         playerCard1Text = playerCard1.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        playerCard2Text = playerCard2.GetComponentInChildren<TMPro.TextMeshProUGUI>();
         dealerCard0Text = dealerCard0.GetComponentInChildren<TMPro.TextMeshProUGUI>();
         dealerCard1Text = dealerCard1.GetComponentInChildren<TMPro.TextMeshProUGUI>();
 
         playerCard0Image = playerCard0.GetComponent<UnityEngine.UI.Image>();
         playerCard1Image = playerCard1.GetComponent<UnityEngine.UI.Image>();
+        playerCard2Image = playerCard2.GetComponent<UnityEngine.UI.Image>();
         dealerCard0Image = dealerCard0.GetComponent<UnityEngine.UI.Image>();
         dealerCard1Image = dealerCard1.GetComponent<UnityEngine.UI.Image>();
 
@@ -87,25 +95,63 @@ public class DeckGenerator : MonoBehaviour
         return null;
     }
 
-    public void BeginRound()
+    public void StartDealing()
+    {
+        StartCoroutine(Deal());
+    }
+
+    IEnumerator Deal()
     {
         if (upcomingCards.Count > 0)
         {
             //players 1st card
             playerCard0Text.text = DrawCard();
             playerCard0Image.color = Color.white;
+            playerCard0Image.enabled = true;
             playerCard0Text.enabled = true;
+            yield return new WaitForSeconds(dealSpeed);
+
             //dealer 1st card (hidden)
             dealerCard0Text.text = DrawCard();
+            dealerCard0Image.enabled = true;
+            yield return new WaitForSeconds(dealSpeed);
+
             //players 2nd card
             playerCard1Text.text = DrawCard();
             playerCard1Image.color = Color.white;
+            playerCard1Image.enabled = true;
             playerCard1Text.enabled = true;
+            yield return new WaitForSeconds(dealSpeed);
+
             //dealer 2nd card
             dealerCard1Text.text = DrawCard();
             dealerCard1Image.color = Color.white;
+            dealerCard1Image.enabled = true;
             dealerCard1Text.enabled = true;
         }
-        
+    }
+
+    public void Hit()
+    {
+        if (upcomingCards.Count > 0)
+        {
+            playerCard2Text.text = DrawCard();
+            playerCard2Image.color = Color.white;
+            playerCard2Image.enabled = true;
+            playerCard2Text.enabled = true;
+        }
+        RevealDealerCard();
+    }
+
+    public void Stand()
+    {
+        RevealDealerCard();
+    }
+
+    private void RevealDealerCard()
+    {
+        dealerCard0Image.color = Color.white;
+        dealerCard0Text.enabled = true;
+        dealerCard0Image.enabled = true;
     }
 }
